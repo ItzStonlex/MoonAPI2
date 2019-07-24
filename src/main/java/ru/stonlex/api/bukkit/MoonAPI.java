f0f2196a2914b5d755a124414cc22f2e9a1b3df9
itzstonlex <itzstonlex@bk.ru>
2019-07-24 12:11:37 +0400

    add packet listener

--- src/main/java/ru/stonlex/api/bukkit/MoonAPI.java
+++ src/main/java/ru/stonlex/api/bukkit/MoonAPI.java
@@ -1,6 +1,13 @@
 package ru.stonlex.api.bukkit;
 
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLib;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.events.ListenerPriority;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketEvent;
 import lombok.Getter;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import ru.stonlex.api.bukkit.commands.CommandManager;
 import ru.stonlex.api.bukkit.event.EventRegisterManager;
@@ -10,8 +17,12 @@ import ru.stonlex.api.bukkit.listeners.PlayerListener;
 import ru.stonlex.api.bukkit.menus.listener.InventoryListener;
 import ru.stonlex.api.bukkit.menus.manager.InventoryManager;
 import ru.stonlex.api.bukkit.messaging.MessagingManager;
+import ru.stonlex.api.bukkit.modules.protocol.entity.MoonFakeEntity;
 import ru.stonlex.api.bukkit.modules.vault.VaultManager;
 import ru.stonlex.api.bukkit.schedulers.SchedulerManager;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public final class MoonAPI extends JavaPlugin {
 
@@ -47,6 +58,35 @@ public final class MoonAPI extends JavaPlugin {
 
         getServer().getPluginManager().registerEvents(new InventoryListener(), this);
         getServer().getPluginManager().registerEvents(new PlayerListener(), this);
+
+        registerProtocolListener();
+    }
+
+    private void registerProtocolListener() {
+        Map<String, Long> cooldowns = new HashMap<>();
+
+        ProtocolLibrary.getProtocolManager().addPacketListener(new PacketAdapter(this, PacketType.Play.Client.USE_ENTITY) {
+            @Override
+            public void onPacketReceiving(PacketEvent e) {
+                Player player = e.getPlayer();
+                int entityId = e.getPacket().getIntegers().read(0);
+
+                if (cooldowns.containsKey(player.getName())
+                        && cooldowns.get(player.getName()) > System.currentTimeMillis()) {
+                    return;
+                }
+
+                MoonFakeEntity fakeEntity = MoonFakeEntity.getEntityById(entityId);
+
+                if (fakeEntity == null || fakeEntity.getClickable() == null) {
+                    return;
+                }
+
+                fakeEntity.getClickable().onClick(player);
+
+                cooldowns.put(player.getName(), System.currentTimeMillis() + 1000);
+            }
+        });
     }
 
 }
